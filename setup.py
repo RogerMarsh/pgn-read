3a3523b3cd12389f4f7e114dbda587975b8e5c44
Roger Marsh <roger.marsh@solentware.co.uk>
2021-04-28 10:28:30 +0000

    pgn-read-2.1 published.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     setup(
         name='pgn-read',
-        version='2.1.dev1',
+        version='2.1',
         description='Portable Game Notation (PGN) parser',
         author='Roger Marsh',
         author_email='roger.marsh@solentware.co.uk',
